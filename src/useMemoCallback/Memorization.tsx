63d539bd123f8fe346f3f37a8d94b2601dc07611
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-11-01 19:35:42 +0530

    implement react memoriazation

---
+++ src/useMemoCallback/Memorization.tsx
@@ -0,0 +1,48 @@
+import React,{useState,useMemo,useCallback} from 'react'
+import Button from '../components/button/Button';
+import Department from '../components/department/Department';
+import Employee from '../components/employee/Employee';
+
+const Memorization = () => {
+    const [name, setName] = useState<string>('Ram Shaoo');
+    const [department, setDepartment] = useState<string>('Back End');
+    const [count, setCount] = useState<number>(0);
+    const [price, setPrice] = useState<number>(0);
+    console.log('home');
+    //useCallBack Implement
+    const handleChange =useCallback(()=>{
+      setName('Suman Jana');
+    },[]);
+  
+    const handleDepartChange=useCallback(()=>{
+      setDepartment('Front-End');
+    },[]);
+    
+    //useMemo Impletement
+    const isOk=useMemo(()=>{
+      console.log('ok checking');
+      return count>5?true:false;
+    },[count])
+    return (
+      <div>
+         <Employee name={name}/>
+         <br />
+        <Button onChange={handleChange}>Update Employee Name</Button>
+        <br />
+        <br />
+        <Department department={department}/>
+        <br />
+        <Button onChange={handleDepartChange}>Update Employee Department</Button>
+        <br />
+        <br />
+        <span>{isOk?'yes':'No'}</span>
+        <br/>
+        <button onClick={()=>setCount(count+1)}>Count-{count}</button>
+        <br/>
+        <button onClick={()=>setPrice(price+1)}>Price-{price}</button>
+      </div>
+    )
+  }
+  
+
+export default Memorization
